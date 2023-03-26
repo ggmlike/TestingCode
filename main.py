0921f1f2f64414107f038fa88eef0ac5d122c5c8
ggmlike <ggmlike@gmail.com>
2023-03-27 01:44:04 +0300

    Determine the eyelid

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-# change of direction
+# Change of direction
 
 def reverse_string(string):
     return string[::-1]
 
+
 print(reverse_string("changeOfDirection"))
+
+# Determine the eyelid
+def determineEyelid(year):
+    century = (year + 99) // 100
+    return century
+print(determineEyelid(2023))
