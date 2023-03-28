a5535d3249c060e5a99c3d7f383739d92c93def3
ggmlike <ggmlike@gmail.com>
2023-03-28 21:15:33 +0300

    fix code and create function sum variables

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-# Change of direction
+# Inversion srting
 def reverse_string(string):
     return string[::-1]
 
 
-print(reverse_string("changeOfDirection"))
+print(reverse_string("change Of Direction"))
 
 
-# Determine the eyelid
-def determin(year):
+# Year and age
+def get_age(year):
     century = (year + 99) // 100
     return century
 
 
-print(determin(2023))
+print(get_age(2023))
+
+
+# sum variables
+def sum_variables(varibles):
+    summer = 0
+    for var in varibles:
+        summer += var
+    return summer
+
+
+print(sum_variables([10, 20, 30, 40, 50, 60]))
